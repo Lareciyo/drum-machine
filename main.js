6cef361594a9ee0c192379b427a217fd7d5e0bee
Lareciyo <lmcneal@perseverenow.org>
2025-07-01 16:52:45 -0500

    first comment

--- main.js
+++ main.js
@@ -1,17 +1,94 @@
-// Setup 'tick' sound
+// Setup 'tick' sound 
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
+const kick = new Audio('sounds/kick-drum.mp3');
+const snare = new Audio('sounds/snare-drum.mp3');
+const hihat = new Audio('sounds/hi-hat.mp3');
+let count = 1;
+let isRunning = true;
+//assigning the count to 1 to start at 1
+function playNextBeat(){
+    if(!isRunning) return;
 
-// This function is called every 600ms
-function update() {
+    document.querySelector('#beat-count').innerText = count;
 
-    // Play the 'tick' sound
-    tick.play();
+    const metronomeOn = document.querySelector('#metronome').checked;
+    if(metronomeOn){
+    if(count === 4){
+        tock.currentTime = 0;
+        tock.play()
+    }else {
+        tick.currentTime = 0;
+        tick.play();
+    }
 }
+
+playInstrument('kick-drum', kick);
+playInstrument('snare-drum', snare);
+playInstrument('hi-hat', hihat);
+
+document.querySelector('.arm').style.animationPlayState = 'running';
+
+count = (count % 4) + 1;
+setTimeout(playNextBeat, 600)
+}
+
+function playInstrument(id, audio){
+    const isChecked = document.querySelector(`#${id}`).checked;
+    const timingInput = document.querySelector(`#${id}-timing`).value;
+
+    if(isChecked && Number(timingInput) === count){
+        audio.currentTime = 0;
+        audio.play();
+}
+}
+
+function start(){
+    isRunning = true;
+    // playNextBeat();
+    }
+
+    function stop(){
+        isRunning = false;
+        document.querySelector('.arm').style.animationPlayState = 'paused';
+
+        }
+
+        document.querySelector('#stop').addEventListener('click', function(){
+            if(isRunning){
+                stop();
+                this.innerText = 'Resume';
+            }else{
+                start();
+                this.innerText = 'Pause';
+            }
+        })
+        document.body.addEventListener('click', () => {
+            tick.play().catch(() => {})
+        })
+
+        setTimeout(start, 300)
 
 // This function sets up update() to be called every 600ms
-function setupUpdate() {
-    setInterval(update, 600);
-}
+
+   
+    function setupUpdate(){
+        setTimeout(setupUpdate, 600);{
+        if(count === 4){
+            tock.play();
+        }else{
+            tick.play()
+        } 
+        count = count % 4 + 1;
+    }
+        function updatetock(){
+        tock.play()
+    }
+    }
+
 
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
+
+
+
